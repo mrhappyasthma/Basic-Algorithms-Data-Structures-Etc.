f0a14a825aa4654e7ce86e46c81131e208471bf0
Mark Klara <mrhappyasthma@gmail.com>
2015-06-27 20:42:28 -0400

    Created StringHashFunction.java
    A modular hash function for strings based off of http://algs4.cs.princeton.edu/34hash/

---
+++ Java-Utilities/StringHashFunction.java
@@ -0,0 +1,8 @@
+int M = SizeOfTable;  //Use the size of the data structure for the table here!
+int hash = 0;
+int R = 31; //per Java -- small prime int
+
+for(int i = 0; i < str.length(); i++)
+{
+  hash = (R * hash + s.charAt(i)) % M;
+}
